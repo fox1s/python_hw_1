6907e548f90638239ab8d123fa8ea31040f5600e
fox1s <ostinsmikh@gmail.com>
2021-02-27 18:24:18 +0200

    hw-3

--- hw_3.py
+++ hw_3.py
@@ -2,45 +2,45 @@
 # создать класс Human(имя и возраст)
 # и два класса Prince и Cinderella которые наследуются от Human
 # у принца должен быть размер туфельки и  метод который принимает лист золушек и выводит какой золушки подошла туфелька
-class Human:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def __repr__(self):
-        return f'{self.name}: {self.age}'
-
-
-class Prince(Human):
-    def __init__(self, name, age, foot_size):
-        super().__init__(name, age)
-        self.foot_size = foot_size
-
-    def is_shoe_fits(self, list_of_cinderellas):
-        for cinderella in list_of_cinderellas:
-            if self.foot_size == cinderella['foot']:
-                return f'{self.name} і {cinderella["name"]} гойра весіллє '
-
-
-class Cinderella(Human):
-    def __init__(self, name, age, foot):
-        super().__init__(name, age)
-        self.foot = foot
-
-
-ira = Cinderella('Ira', 16, 35).__dict__
-katya = Cinderella('Katya', 23, 41).__dict__
-olya = Cinderella('Olya', 22, 38).__dict__
-khrystyna = Cinderella('Khrystyna', 20, 36).__dict__
-vika = Cinderella('Vika', 17, 36).__dict__
-sophia = Cinderella('Sophia', 21, 37).__dict__
-
-list_of_cinderellas = [ira, katya, olya, khrystyna, vika, sophia]
-
-rostyslav = Prince('Rostyslav', 20, 36)
-
-love_story = Prince.is_shoe_fits(rostyslav, list_of_cinderellas)
-print(love_story)
+# class Human:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def __repr__(self):
+#         return f'{self.name}: {self.age}'
+#
+#
+# class Prince(Human):
+#     def __init__(self, name, age, foot_size):
+#         super().__init__(name, age)
+#         self.foot_size = foot_size
+#
+#     def is_shoe_fits(self, list_of_cinderellas):
+#         for cinderella in list_of_cinderellas:
+#             if self.foot_size == cinderella['foot']:
+#                 return f'{self.name} і {cinderella["name"]} гойра весіллє '
+#
+#
+# class Cinderella(Human):
+#     def __init__(self, name, age, foot):
+#         super().__init__(name, age)
+#         self.foot = foot
+#
+#
+# ira = Cinderella('Ira', 16, 35).__dict__
+# katya = Cinderella('Katya', 23, 41).__dict__
+# olya = Cinderella('Olya', 22, 38).__dict__
+# khrystyna = Cinderella('Khrystyna', 20, 39).__dict__
+# vika = Cinderella('Vika', 17, 36).__dict__
+# sophia = Cinderella('Sophia', 21, 37).__dict__
+#
+# list_of_cinderellas = [ira, katya, olya, khrystyna, vika, sophia]
+#
+# rostyslav = Prince('Rostyslav', 21, 39)
+#
+# love_story = Prince.is_shoe_fits(rostyslav, list_of_cinderellas)
+# print(love_story)
 
 
 # =================================================================
@@ -61,4 +61,37 @@ class Rectangle:
         self.y = y
 
     def __add__(self, other):
-        return self.P + other.P
+        return (self.x * self.y) + (other.x * other.y)
+
+    def __sub__(self, other):
+        return (self.x * self.y) - (other.x * other.y)
+
+    def __eq__(self, other):
+        return (self.x * self.y) == (other.x * other.y)
+
+    def __ne__(self, other):
+        return (self.x * self.y) != (other.x * other.y)
+
+    def __gt__(self, other):
+        return (self.x * self.y) > (other.x * other.y)
+
+    def __lt__(self, other):
+        return (self.x * self.y) < (other.x * other.y)
+
+    def __len__(self):
+        return self.x + self.y
+
+
+rectangle1 = Rectangle(5, 2)
+rectangle2 = Rectangle(3, 2)
+
+print(f'1. x = {rectangle1.x}, y = {rectangle1.y}, S1 = {rectangle1.x * rectangle1.y} \n'
+      f'2. x = {rectangle2.x}, y = {rectangle2.y}, S2= {rectangle2.x * rectangle2.y} \n')
+
+print('Sum:', rectangle1 + rectangle2)
+print('Sub:', rectangle1 - rectangle2)
+print('Is equal:', rectangle1 == rectangle2)
+print('Is not equal:', rectangle1 != rectangle2)
+print('Is rectangle1 greater then rectangle2:', rectangle1 > rectangle2)
+print('Is rectangle2 greater then rectangle1:', rectangle1 < rectangle2)
+print('Length of one rectangle:', len(rectangle1))
